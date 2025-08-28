44baeec9996cc58ff3705df79914eeaf665e2c5b
CarolineRengel1502 <145135160+CarolineRengel1502@users.noreply.github.com>
2025-08-28 11:34:18 -0300

    kk

--- script.js
+++ script.js
@@ -3,3 +3,55 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "1- A inteligência artificial será cada vez mais usada na educação para o ensino?",
+        alternativas: [
+            "Sim",
+            "Não"
+        ]
+    },
+]
+{
+    enunciado: "2- A IA pode substituir totalmente  os humanos em todas as profissões futuras?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
+{
+    enunciado: "3- É possível que a IA desenvolva consciência própria como um ser humano?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
+{
+    enunciado: "4- A IA pode ajudar a reduzir desiguadades sociais por meio de decisões justas e imparciais?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
+{
+    enunciado: "5- A inteligência artificial terá um papel importante no combate ás mudanças climáticas?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
+{
+    enunciado: "6- A Inteligência Artificial será capaz de tomar decisões éticas sozinhos, sem intervenção humana?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
+{
+    enunciado: "7- No futuro, os robôs com inteligência Artificial poderão cuidar de idosos e pessoas com deficiência?",
+    alternativas: [
+        "Sim",
+        "Não"
+    ]
+}
