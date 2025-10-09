26e32d8b467d7e689f9ba6ce09f0367a1e995c50
CarolineRengel1502 <145135160+CarolineRengel1502@users.noreply.github.com>
2025-10-09 11:34:00 -0300

    ok

--- script.js
+++ script.js
@@ -12,53 +12,41 @@ const perguntas = [
            "Não" 
         ]
     },
-]
-[
 {
     enunciado: "2- A IA pode substituir totalmente  os humanos em todas as profissões futuras?",
     alternativas: [
         "Sim",
         "Não"
     ]
-}
-]
-[
+},
 {
     enunciado: "3- É possível que a IA desenvolva consciência própria como um ser humano?",
     alternativas: [
         "Sim",
         "Não"
     ]
-}
-]
-[
+},
 {
     enunciado: "4- A IA pode ajudar a reduzir desiguadades sociais por meio de decisões justas e imparciais?",
     alternativas: [
         "Sim",
         "Não"
     ]
-}
-]
-[
+},
 {
     enunciado: "5- A inteligência artificial terá um papel importante no combate ás mudanças climáticas?",
     alternativas: [
         "Sim",
         "Não"
     ]
-}
-]
-[
+},
 {
     enunciado: "6- A Inteligência Artificial será capaz de tomar decisões éticas sozinhos, sem intervenção humana?",
     alternativas: [
         "Sim",
         "Não"
     ]
-}
-]
-[
+},
 {
     enunciado: "7- No futuro, os robôs com inteligência Artificial poderão cuidar de idosos e pessoas com deficiência?",
     alternativas: [
